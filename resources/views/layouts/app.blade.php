4cbe135696e100213085d8454f7295204c0e84f4
Rama <ramap0346@gmail.com>
2026-09-22 11:59:59 +0800

    fix(csp): tambahkan meta tag upgrade-insecure-requests untuk melarang pengiriman via HTTP

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>@yield('title', 'Sistem Realisasi Retribusi') - BAPENDA</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
