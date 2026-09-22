a1c40ab679908f5e1411198358699b8a70beb545
Rama <ramap0346@gmail.com>
2026-09-22 12:06:36 +0800

    fix(https): paksa HTTPS tanpa syarat di Laravel level dan konfigurasi lengkap TrustProxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,8 +22,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrapFive();
 
-        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https' || env('APP_ENV') === 'production') {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        \Illuminate\Support\Facades\URL::forceScheme('https');
+        $this->app['request']->server->set('HTTPS', 'on');
     }
 }
